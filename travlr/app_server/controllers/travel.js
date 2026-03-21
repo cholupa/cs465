676a061b1c746e000e295a69582947fb06130ada
pootytang <dshing.hui@proton.me>
2026-03-20 18:18:29 -0700

    Module 3 completed

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,8 @@
 // controller for travel page
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync("./data/trips.json"))
 const travel = (req,res)=>{
-    res.render('travel', {title: 'Travel Page'});
+    res.render('travel', {title: 'Travel Page', trips});
 }
 
 module.exports = {
